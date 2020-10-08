b7227f237e0c0f43b24609667df058270da3ee9d
Diego Bosch <diegojbosch@gmail.com>
2020-10-08 18:15:34 -0300

    Get stock information from backend

--- application.py
+++ application.py
@@ -6,13 +6,12 @@ application = Flask(__name__)
 if __name__ == '__main__':
 	application.run(debug=True)
 
+with open("token.txt", "r") as file:
+	tiingo_api_token = file.readline()
 
-#retrieve company outlook information
+# retrieve company outlook information
 @application.route('/api/v1.0/company-outlook', methods=['GET'])
 def get_company_outlook():
-
-	with open("token.txt", "r") as file:
-		tiingo_api_token = file.readline()
 
 	if request.args.get('stock_ticker') is not None:
 		stock_ticker = request.args.get('stock_ticker')
@@ -30,6 +29,32 @@ def get_company_outlook():
 		"description": resp_json['description']
 	}
 
-	print resp_json['description']
-
 	return jsonify(company_outlook)
+
+
+# retrieve stock information for the ticker
+@application.route('/api/v1.0/stock-information', methods=['GET'])
+def get_stock_information():
+
+	if request.args.get('stock_ticker') is not None:
+		stock_ticker = request.args.get('stock_ticker')
+
+	tiingo_api_url = 'https://api.tiingo.com/iex/' + stock_ticker + '?token=' + tiingo_api_token
+
+	response = requests.get(tiingo_api_url)
+	resp_json = response.json()
+
+	stock_information = {
+		"stockTickerSymbol": resp_json[0]['ticker'],
+		"tradingDay": resp_json[0]['timestamp'],
+		"previousClosingPrice": resp_json[0]['prevClose'],
+		"openingPrice": resp_json[0]['open'],
+		"highPrice": resp_json[0]['high'],
+		"lowPrice": resp_json[0]['low'],
+		"lastPrice": resp_json[0]['last'],
+		"change": resp_json[0]['last'] - resp_json[0]['prevClose'],
+		"changePercent": ((resp_json[0]['last'] - resp_json[0]['prevClose']) / resp_json[0]['prevClose']) * 100,
+		"numberSharesTraded": resp_json[0]['volume']
+	}
+
+	return jsonify(stock_information)
